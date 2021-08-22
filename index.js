3de1128a4f406739f0408c14ca0a798570d53791
Sergey Aldoukhov <saldoukhov@gmail.com>
2021-08-22 16:30:08 -0700

    use ksm

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+const {
+    getSecrets,
+    localConfigStorage
+} = require('@keeper-security/secrets-manager-core')
+
+const getKeeperRecords = async () => {
+    const storage = localConfigStorage("config.json")
+    const {records} = await getSecrets({storage: storage})
+}
+
 try {
     const config = core.getInput('keeper-secret-config');
     console.log(`Config: ${config}`);
-    core.setOutput("secret", 'bla bla');
+    getKeeperRecords().then(x => core.setOutput("secret", x[0]))
     // Get the JSON webhook payload for the event that triggered the workflow
     // const payload = JSON.stringify(github.context.payload, undefined, 2)
     // console.log(`The event payload: ${payload}`);
