6e5662ba5e1f04122bc15ab1b540c22445593c35
Sergey Aldoukhov <saldoukhov@gmail.com>
2021-08-25 18:02:12 -0700

    debug

--- index.js
+++ index.js
@@ -8,14 +8,18 @@ const {
 async function run() {
     try {
         const configJson = core.getInput('keeper-secret-config');
+        const secrets = core.getInput('secrets');
+        console.log(secrets)
         const storage = loadJsonConfig(configJson)
         const {records} = await getSecrets({storage: storage})
         console.log(records)
-        core.setOutput("secret", records[0])
+        const record = records.find(x => x.recordUid === 'BediNKCMG21ztm5xGYgNww')
+        console.log(record)
+        const value = record.fields.find(x => x.type === 'password')
+        console.log(value)
+        core.setOutput("secret", value)
     } catch (error) {
         core.setFailed(error.message);
     }
 }
-
-core.setOutput("secret2", "bla bla")
 run()
