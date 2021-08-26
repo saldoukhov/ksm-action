a6b15b79de6e6b5dccf8aaa7b57405bb58b6bc1b
Sergey Aldoukhov <saldoukhov@gmail.com>
2021-08-25 17:02:47 -0700

    debug

--- dist/index.js
+++ dist/index.js
@@ -6958,19 +6958,16 @@ const {
 const getKeeperRecords = async (configJson) => {
     const storage = loadJsonConfig(configJson)
     const {records} = await getSecrets({storage: storage})
+    console.log(records)
     return records[0]
 }
 
 try {
     const config = core.getInput('keeper-secret-config');
-    console.log(`Config: ${config}`);
-    console.log(`Config Length: ${config.length}`);
-    const configObj = JSON.parse(config)
-    console.log('Config parsed:', configObj);
-    getKeeperRecords(config).then(x => core.setOutput("secret", x[0]))
+    getKeeperRecords(config).then(x => core.setOutput("secret", x[0])).finally()
     // Get the JSON webhook payload for the event that triggered the workflow
-    // const payload = JSON.stringify(github.context.payload, undefined, 2)
-    // console.log(`The event payload: ${payload}`);
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
 } catch (error) {
     core.setFailed(error.message);
 }
