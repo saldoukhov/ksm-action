450ee2a477e194c109df0ced184375adae4bf56b
Sergey Aldoukhov <saldoukhov@gmail.com>
2021-08-25 17:46:46 -0700

    debug

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 
 const {
     getSecrets,
     loadJsonConfig
 } = require('@keeper-security/secrets-manager-core')
 
-const getKeeperRecords = async (configJson) => {
-    const storage = loadJsonConfig(configJson)
-    const {records} = await getSecrets({storage: storage})
-    console.log(records)
-    return records[0]
+async function run() {
+    try {
+        const configJson = core.getInput('keeper-secret-config');
+        const storage = loadJsonConfig(configJson)
+        const {records} = await getSecrets({storage: storage})
+        console.log(records)
+        core.setOutput("secret", records[0])
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
 
-try {
-    const config = core.getInput('keeper-secret-config');
-    getKeeperRecords(config).then(x => core.setOutput("secret", x[0])).finally()
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
-} catch (error) {
-    core.setFailed(error.message);
-}
+core.setOutput("secret2", "bla bla")
+run()
